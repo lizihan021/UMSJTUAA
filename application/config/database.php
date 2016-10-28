9de129da87cd36c2dca6d5bf36e666f83d6da78d
Zihan Li <lizihan@umich.edu>
2016-10-28 15:12:52 -0400

    to hide database hide password and username

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'www.jikaisa.net',
-	'username' => 'saa',
-	'password' => 'lizihan',
-	'database' => 'test',
+	'hostname' => '',
+	'username' => '',
+	'password' => '',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
