2031092af2696cc032056faed77cc2b1ef175245
Zihan Li <lizihan@umich.edu>
2016-10-29 10:26:51 -0400

    change the dirctory add Admin controller

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,31 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model('db_model');
+		$this->load->helper('url');
+		$this->output->enable_profiler(TRUE);
 	}
 
 	public function index()
 	{
+
 		$data['site_name'] = $this->db_model->get_site_name();
-		$this->load->view('welcome_message',$data);
+		$this->load->view('home');
+
 
 	}
 }
--- application/views/home.php
+++ application/views/home.php
@@ -3,10 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	
+
+    <title> SAA Website </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Bootstrap -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This is some text.</p> 
+</div>
 
 </body>
 </html>
